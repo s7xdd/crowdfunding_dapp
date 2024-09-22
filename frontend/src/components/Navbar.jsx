beb8609cdd01741599e5de33dab83a137945367d
Mohammed <kmshamil123@gmail.com>
2024-09-23 00:04:36 +0530

    Fixed create campaign button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,32 +1,38 @@
-import { Button, ButtonGroup, Link } from '@chakra-ui/react'
+import { Button, ButtonGroup, Link } from "@chakra-ui/react";
 import { FaPencilAlt } from "react-icons/fa";
-import '../styles/Navbar.css'
-import { useState } from 'react';
+import "../styles/Navbar.css";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-  const Navbar = () => {
-    const [wallet, setWallet] = useState(false)
-    return (
-      <div className='navbar'>
-        <div>
-          <Link 
-            className='title' href={'/'}
-            style={{fontSize: "25px"}}
-          >Crowdfund</Link>
-        </div>
-        <div style={{display: 'flex', gap: '10px'}}>
-          <Button className='create_campaign' colorScheme='blue'><FaPencilAlt />Create Campaign</Button>
-          { wallet ?
-          (
-            <h1>182311...</h1>
-          )
-          : (
-            <>
-            <Button className='connect_wallet' colorScheme='blue'><FaPencilAlt />Connect Wallet</Button>
-            </>
-          )}
-        </div>
+const Navbar = () => {
+  const [wallet, setWallet] = useState(false);
+  return (
+    <div className="navbar">
+      <div>
+        <Link className="title" href={"/"} style={{ fontSize: "25px" }}>
+          Crowdfund
+        </Link>
       </div>
-    )
-  }
-  
-  export default Navbar
+      <div style={{ display: "flex", gap: "10px" }}>
+        <NavLink to={"/create"}>
+          <Button className="create_campaign" colorScheme="blue">
+            <FaPencilAlt />
+            Create Campaign
+          </Button>
+        </NavLink>
+        {wallet ? (
+          <h1>182311...</h1>
+        ) : (
+          <>
+            <Button className="connect_wallet" colorScheme="blue">
+              <FaPencilAlt />
+              Connect Wallet
+            </Button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
