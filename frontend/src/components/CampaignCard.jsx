581d9df6da4916b434f718e7860f37facc6dd485
Mohammed <kmshamil123@gmail.com>
2024-09-23 22:02:18 +0530

    Added filters in home page and updated campaign card

--- frontend/src/components/CampaignCard.jsx
+++ frontend/src/components/CampaignCard.jsx
@@ -1,66 +1,88 @@
-import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Flex, GridItem, Heading, Image, Progress, Stack, Text } from '@chakra-ui/react'
-import React from 'react'
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Flex,
+  GridItem,
+  Heading,
+  Image,
+  Progress,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
 import { FaClock } from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const CampaignCard = () => {
   return (
     <GridItem w="100%">
-          <Card maxW="sm">
-            <CardBody>
-              <NavLink to={`/campaigns/livingroomsofa`}>
-              <Image
-                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Green double couch with wooden legs"
-                borderRadius="lg"
-                />
-                </NavLink>
-              <Stack mt="6" spacing="3">
-                <Text backgroundColor='lightgreen' width={70} rounded='full' align='center'>Active</Text>
-                <Heading size="md">Living room Sofa</Heading>
-                <Text>
-                  This sofa is perfect for modern tropical spaces, baroque
-                  inspired spaces.
-                </Text>
-                <Flex align="center" gap={2}>
-                  <Text color="blue.600" fontSize="lg" fontWeight="bold">
-                    0.1
-                  </Text>
-                  <Text fontSize="sm">ETH funded | Total : 10 ETH</Text>
-                </Flex>
+      <Card maxW="sm">
+        <CardBody>
+          <NavLink to={`/campaigns/livingroomsofa`}>
+            <Image
+              src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt="Green double couch with wooden legs"
+              borderRadius="lg"
+            />
+          </NavLink>
+          <Stack mt="6" spacing="3">
+            <Flex justify="space-between">
+              <Text
+                backgroundColor="lightgreen"
+                width={70}
+                rounded="full"
+                align="center"
+              >
+                Active
+              </Text>
+              <Text>243 donations</Text>
+            </Flex>
+            <Heading size="md">Living room Sofa</Heading>
+            <Text>
+              This sofa is perfect for modern tropical spaces, baroque inspired
+              spaces.
+            </Text>
+            <Flex align="center" gap={2}>
+              <Text color="blue.600" fontSize="lg" fontWeight="bold">
+                0.1
+              </Text>
+              <Text fontSize="sm">ETH funded | Total : 10 ETH</Text>
+            </Flex>
 
-                <Box>
-                  <Flex align="center">
-                    <Progress value={80} flex="1" mr={3} />
-                    <Text>80%</Text>
-                  </Flex>
-                </Box>
+            <Box>
+              <Flex align="center">
+                <Progress value={80} flex="1" mr={3} />
+                <Text>80%</Text>
+              </Flex>
+            </Box>
 
-                <Box>
-                    <Flex gap={3} align='center'>
-                    <FaClock />
-                    <Text>
-                         2 days left
-                    </Text>
-                    </Flex>
-                </Box>
-                
-              </Stack>
-            </CardBody>
-            <Divider />
-            <CardFooter>
-              <ButtonGroup spacing="2">
-                <Button variant="solid" colorScheme="blue">
-                  Read more
-                </Button>
-                <Button variant="ghost" colorScheme="blue">
-                  Fund
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
-          </Card>
-        </GridItem>
-  )
-}
+            <Box>
+              <Flex gap={3} align="center">
+                <FaClock />
+                <Text>2 days left</Text>
+              </Flex>
+            </Box>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <ButtonGroup spacing="2">
+            <Button variant="solid" colorScheme="blue">
+              Read more
+            </Button>
+            <Button variant="ghost" colorScheme="blue">
+              Fund
+            </Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
+    </GridItem>
+  );
+};
 
-export default CampaignCard
+export default CampaignCard;
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -9,6 +9,7 @@ import {
   Highlight,
   Image,
   Input,
+  Stack,
   Text,
 } from "@chakra-ui/react";
 import React from "react";
@@ -53,24 +54,70 @@ const Homepage = () => {
       </div>
 
       <Box>
-        <Center flexDirection='column'>
+        <Center flexDirection="column">
           <Flex gap="7px" mt="60px" mb="30px" fontSize="32px">
             <Text color="black" fontWeight="bold">
               Open
             </Text>{" "}
             <Text color="#13ADB7">Donations</Text>
           </Flex>
-          <Box border='1px solid' display='flex' alignItems='center' rounded='full' dropShadow='2'>
-            <Input border='0' width='500px' rounded='full' placeholder="Find donations" />
-            <CiSearch size='25'/>
+          <Box
+            border="1px solid"
+            display="flex"
+            alignItems="center"
+            rounded="full"
+            dropShadow="2"
+          >
+            <Input
+              border="0"
+              width="500px"
+              rounded="full"
+              placeholder="Find donations"
+            />
+            <CiSearch size="25" />
           </Box>
+
+          <Stack direction='row' spacing={5} align='center' mt={7} >
+            <Button colorScheme="teal" variant="solid" rounded='full'>
+              All
+            </Button>
+            <Button colorScheme="teal" variant="outline" rounded='full'>
+              Disaster
+            </Button>
+            <Button colorScheme="teal" variant="outline" rounded='full'>
+              Children
+            </Button>
+            <Button colorScheme="teal" variant="outline" rounded='full'>
+              Food Crisis
+            </Button>
+            <Button colorScheme="teal" variant="outline" rounded='full'>
+              Health
+            </Button>
+            <Button colorScheme="teal" variant="outline" rounded='full'>
+              Education
+            </Button>
+            <Button colorScheme="teal" variant="outline" rounded='full'>
+              Homeless
+            </Button>
+            <Button colorScheme="teal" variant="outline" rounded='full'>
+              Animal
+            </Button>
+            <Button colorScheme="teal" variant="outline" rounded='full'>
+              Pandemic
+            </Button>
+            <Button colorScheme="teal" variant="outline" rounded='full'>
+              War Crisis
+            </Button>
+          </Stack>
         </Center>
 
         <Box>
           <Grid
             templateColumns={{ lg: "repeat(4, 1fr)", md: "repeat(2, 1fr)" }}
-            gap={6}
-            mt={10}
+            rowGap={10}
+            pl={20}
+            pt={10}
+            pr={20}
           >
             <CampaignCard />
             <CampaignCard />
